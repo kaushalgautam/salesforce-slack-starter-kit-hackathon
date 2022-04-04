302c2763ca3dd5bc04ffa02a736a8fa91aeadbff
Kaushal Gautam <kaushalgautam.alt@gmail.com>
2022-04-05 01:57:00 +0530

    adding modal

--- apps/salesforce-slack-app/listeners/shortcuts/queryPTOSummary.js
+++ apps/salesforce-slack-app/listeners/shortcuts/queryPTOSummary.js
@@ -1,3 +1,5 @@
+const { Modal, Blocks } = require('slack-block-builder');
+
 const getPTOSummary = async ({ shortcut, ack, client, context }) => {
     // shortcut is getting called
     // client = use slack api
@@ -25,10 +27,64 @@ const getPTOSummary = async ({ shortcut, ack, client, context }) => {
             if (err) {
                 return console.error(err);
             }
-            console.log('response: ', res);
-            // the response object structure depends on the definition of apex class
+            let ptoSummaryText = '';
+            let projectWisePTOs = {};
+            if (res.lenght > 0) {
+                res.forEach((pto) => {
+                    if (!projectWisePTOs[pto.ProjectName]) {
+                        projectWisePTOs[pto.ProjectName] = [];
+                    }
+                    projectWisePTOs[pto.ProjectName].push({
+                        Name: pto.PTOEntry.Owner.Name,
+                        NumberOfDays: pto.PTOEntry.No_of_PTO_Days__c,
+                        From: pto.PTOEntry.Start_Date__c,
+                        To: pto.PTOEntry.End_Date__c
+                    });
+                });
+
+                console.log(projectWisePTOs);
+
+                for (const proj in projectWisePTOs) {
+                    ptoSummaryText += `:file_folder: ${proj}: \n`;
+                    projectWisePTOs[proj].forEach((ptoEntry) => {
+                        ptoSummaryText += `:calendar: ${ptoEntry.Name} is on PTO for ${ptoEntry.NumberOfDays} days from ${ptoEntry.From} to ${ptoEntry.To}. \n`;
+                    });
+                }
+
+                return Modal({ title: 'PTO Summary' })
+                    .callbackId('showPTOSummary')
+                    .blocks(
+                        Blocks.Section({
+                            text: "Hey there! Here's a summary of your teammates' PTOs "
+                        })
+                    )
+                    .blocks([
+                        Blocks.Divider(),
+                        Blocks.Section({ text: ptoSummaryText })
+                    ])
+                    .buildToJSON();
+            } else {
+                return Modal({ title: 'PTO Summary' })
+                    .callbackId('showPTOSummary')
+                    .blocks(
+                        Blocks.Section({
+                            text: 'None of your teammates have any upcoming PTOs. Maybe you should be on one :palm_tree:'
+                        })
+                    )
+                    .buildToJSON();
+            }
         }
     );
+
+    return Modal({ title: 'Edit Team' })
+        .callbackId('showPTOSummary')
+        .blocks(
+            Blocks.Section({
+                text: "Hey there! Here's a summary of your teammates' PTOs "
+            })
+        )
+        .blocks([Blocks.Divider(), Blocks.Section({ text: ptoSummaryText })])
+        .buildToJSON();
 };
 
 module.exports = { getPTOSummary };
