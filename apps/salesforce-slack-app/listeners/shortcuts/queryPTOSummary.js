99ca2528e02c8edd2fdb58fae969b615d6204306
Kaushal Gautam <kaushalgautam.alt@gmail.com>
2022-04-05 02:19:48 +0530

    bugfix

--- apps/salesforce-slack-app/listeners/shortcuts/queryPTOSummary.js
+++ apps/salesforce-slack-app/listeners/shortcuts/queryPTOSummary.js
@@ -27,57 +27,62 @@ const getPTOSummary = async ({ shortcut, ack, client, context }) => {
             if (err) {
                 return console.error(err);
             }
-            console.log(res);
-            let ptoSummaryText = '';
-            let projectWisePTOs = {};
-            if (res.lenght > 0) {
-                res.forEach((pto) => {
-                    if (!projectWisePTOs[pto.ProjectName]) {
-                        projectWisePTOs[pto.ProjectName] = [];
-                    }
-                    projectWisePTOs[pto.ProjectName].push({
-                        Name: pto.PTOEntry.Owner.Name,
-                        NumberOfDays: pto.PTOEntry.No_of_PTO_Days__c,
-                        From: pto.PTOEntry.Start_Date__c,
-                        To: pto.PTOEntry.End_Date__c
-                    });
-                });
-
-                console.log(projectWisePTOs);
-
-                for (const proj in projectWisePTOs) {
-                    ptoSummaryText += `:file_folder: ${proj}: \n`;
-                    projectWisePTOs[proj].forEach((ptoEntry) => {
-                        ptoSummaryText += `:calendar: ${ptoEntry.Name} is on PTO for ${ptoEntry.NumberOfDays} days from ${ptoEntry.From} to ${ptoEntry.To}. \n`;
-                    });
-                }
-
-                let view = Modal({ title: 'PTO Summary' })
-                    .callbackId('showPTOSummary')
-                    .blocks(
-                        Blocks.Section({
-                            text: "Hey there! Here's a summary of your teammates' PTOs "
-                        })
-                    )
-                    .blocks([
-                        Blocks.Divider(),
-                        Blocks.Section({ text: ptoSummaryText })
-                    ])
-                    .buildToJSON();
-
-                client.views.open({
-                    // Use the user ID associated with the shortcut
-                    trigger_id: shortcut.trigger_id,
-                    view: myTravelRequestsScreen(
-                        travelRequests,
-                        currentuser.display_name,
-                        conn.instanceUrl
-                    )
-                });
-            } else {
-                console.log('something went wrong');
-                console.log('res');
+            try {
                 console.log(res);
+                console.log(res.length);
+                let ptoSummaryText = '';
+                let projectWisePTOs = {};
+                if (res.length > 0) {
+                    res.forEach((pto) => {
+                        if (!projectWisePTOs[pto.ProjectName]) {
+                            projectWisePTOs[pto.ProjectName] = [];
+                        }
+                        projectWisePTOs[pto.ProjectName].push({
+                            Name: pto.PTOEntry.Owner.Name,
+                            NumberOfDays: pto.PTOEntry.No_of_PTO_Days__c,
+                            From: pto.PTOEntry.Start_Date__c,
+                            To: pto.PTOEntry.End_Date__c
+                        });
+                    });
+
+                    console.log(projectWisePTOs);
+
+                    for (const proj in projectWisePTOs) {
+                        ptoSummaryText += `:file_folder: ${proj}: \n`;
+                        projectWisePTOs[proj].forEach((ptoEntry) => {
+                            ptoSummaryText += `:calendar: ${ptoEntry.Name} is on PTO for ${ptoEntry.NumberOfDays} days from ${ptoEntry.From} to ${ptoEntry.To}. \n`;
+                        });
+                    }
+
+                    let view = Modal({ title: 'PTO Summary' })
+                        .callbackId('showPTOSummary')
+                        .blocks(
+                            Blocks.Section({
+                                text: "Hey there! Here's a summary of your teammates' PTOs "
+                            })
+                        )
+                        .blocks([
+                            Blocks.Divider(),
+                            Blocks.Section({ text: ptoSummaryText })
+                        ])
+                        .buildToJSON();
+
+                    client.views.open({
+                        // Use the user ID associated with the shortcut
+                        trigger_id: shortcut.trigger_id,
+                        view: myTravelRequestsScreen(
+                            travelRequests,
+                            currentuser.display_name,
+                            conn.instanceUrl
+                        )
+                    });
+                } else {
+                    console.log('something went wrong');
+                    console.log('res');
+                    console.log(res);
+                }
+            } catch (error) {
+                console.error(error);
             }
         }
     );
