6db2c0fe459b9d4247f83657cdade65d27b701df
Kaushal Gautam <kaushalgautam.alt@gmail.com>
2022-04-19 15:07:26 +0530

    missing import

--- apps/salesforce-slack-app/user-interface/app-home/index.js
+++ apps/salesforce-slack-app/user-interface/app-home/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const { authorizationScreen } = require('./authorization-screen');
 const { authorizationSuccessScreen } = require('./auth-success');
+const { myPTOsScreen } = require('./my-pto-screen');
 
 module.exports = {
     authorizationScreen,
