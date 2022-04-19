1e513caf6baff5ee02ac4fd229ee73d8fa847445
Kaushal Gautam <kaushalgautam.alt@gmail.com>
2022-04-19 15:30:51 +0530

    adding view btn callback

--- apps/salesforce-slack-app/listeners/actions/index.js
+++ apps/salesforce-slack-app/listeners/actions/index.js
@@ -1,6 +1,8 @@
 'use strict';
+const { appHomeViewPtoButtonCallback } = require('./app--home-view-pto-btn');
 const { appHomeAuthorizeButtonCallback } = require('./app-home-authorize-btn');
 
 module.exports.register = (app) => {
     app.action('authorize-with-salesforce', appHomeAuthorizeButtonCallback);
+    app.action('view-pto', appHomeViewPtoButtonCallback);
 };
